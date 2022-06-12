ea20dbbfa6d9fd7217e482fd72bb068afa216b1c
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-06-12 16:50:12 +0800

    Hats to you.

--- src/burger/YAUM.kt
+++ src/burger/YAUM.kt
@@ -2,6 +2,7 @@ package burger
 
 import arc.*
 import arc.util.*
+import mindustry.*
 import mindustry.game.EventType.*
 import mindustry.mod.*
 import mindustry.ui.dialogs.*
@@ -10,12 +11,19 @@ import burger.ui.*
 import burger.game.*
 
 class YAUM : Mod(){
-
+    
+    var ai = AISwitcherDialog()
+    
     init{
         Events.on(ClientLoadEvent::class.java){
             UtilityTable.load()
             Healthbars.load()
             FactoryProgressBars.load()
+        }
+        Events.run(Trigger.update){
+            if(ai.active){
+                Core.camera.position.set(Vars.player.unit().x, Vars.player.unit().y)
+            }
         }
     }
 
